ee8bd355fc2186962ddc88089ceb1898075b5197
shaune <theshaune@users.noreply.github.com>
2016-03-01 16:03:03 +1100

    Add Packages

--- route.js
+++ route.js
@@ -1,5 +1,11 @@
+/* Packages */
+var express = require('express')
+  , router = express.Router()
+
+/* Local Variables */ 
 var categories = '(store|products)';
 
+/* Routes */ 
 router.get('/:category('+categories+')/:slug', function(req, res, next) {
  // Do something
  // req.param.category;
